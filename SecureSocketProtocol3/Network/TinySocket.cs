c1a6d3c18e5c3de6fa7d22af91eee53f6038b44c
DragonHunter <dragoncodehunter@gmail.com>
2016-01-20 21:07:46 +0100

    Add GPLv3 License

--- SecureSocketProtocol3/Network/TinySocket.cs
+++ SecureSocketProtocol3/Network/TinySocket.cs
@@ -5,6 +5,24 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 
+/*
+    Secure Socket Protocol
+    Copyright (C) 2016 AnguisCaptor
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 namespace SecureSocketProtocol3.Network
 {
     public abstract class TinySocket
@@ -157,6 +175,7 @@ namespace SecureSocketProtocol3.Network
 
                 if (Buffer.Length - WriteOffset > 0)
                 {
+                    //socket.BeginReceive(this.Buffer, WriteOffset, 1, SocketFlags.None, AynsReceive, null);
                     socket.BeginReceive(this.Buffer, WriteOffset, Buffer.Length - WriteOffset, SocketFlags.None, AynsReceive, null);
                 }
                 else
